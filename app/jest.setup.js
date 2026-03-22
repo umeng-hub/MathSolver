3cdf255fa43f7d6d879a76f4c7c227799488f297
ROMMEL CATAPANG <umeng1234@gmail.com>
2026-03-21 20:34:06 -0400

    commit

--- app/jest.setup.js
+++ app/jest.setup.js
@@ -1,1 +1,11 @@
 import '@testing-library/jest-native/extend-expect';
+
+jest.mock('@expo/vector-icons', () => {
+	const React = require('react');
+	const { Text } = require('react-native');
+
+	return {
+		MaterialCommunityIcons: ({ name, ...props }) =>
+			React.createElement(Text, props, name || 'icon'),
+	};
+});
